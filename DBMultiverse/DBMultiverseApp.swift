6b4bb74ce042b3bd86c0203ef5964071ac966f89
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-11-10 19:06:21 -0800

    initial commit

---
+++ DBMultiverse/DBMultiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBMultiverseApp.swift
+//  DBMultiverse
+//
+//  Created by Nikolai Nobadi on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DBMultiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

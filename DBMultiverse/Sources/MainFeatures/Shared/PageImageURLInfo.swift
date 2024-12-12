ef4155ebe4c9b4d901779c40a0843daf713653bd
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-12-11 17:06:03 -0800

    deleted old code

---
+++ DBMultiverse/Sources/MainFeatures/Shared/PageImageURLInfo.swift
@@ -0,0 +1,14 @@
+//
+//  PageImageURLInfo.swift
+//  DBMultiverse
+//
+//  Created by Nikolai Nobadi on 12/11/24.
+//
+
+import Foundation
+
+struct PageImageURLInfo {
+    let url: URL?
+    let chapter: String
+    let pageNumber: String
+}

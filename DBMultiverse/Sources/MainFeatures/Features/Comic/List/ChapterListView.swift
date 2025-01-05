35615ee6d4902e51c3dc8f5ed934f26de98f5576
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-01-04 18:20:50 -0800

    refactored Settings view to adjust for iPad

--- DBMultiverse/Sources/MainFeatures/Features/Comic/List/ChapterListView.swift
+++ DBMultiverse/Sources/MainFeatures/Features/Comic/List/ChapterListView.swift
@@ -34,27 +34,6 @@ struct ChapterListView: View {
 }
 
 
-// MARK: - Section
-fileprivate struct DynamicSection<Content: View>: View {
-    let title: String
-    let content: () -> Content
-    
-    init(_ title: String, @ViewBuilder content: @escaping () -> Content) {
-        self.title = title
-        self.content = content
-    }
-    
-    var body: some View {
-        Section {
-            content()
-        } header: {
-            Text(title)
-                .withFont()
-        }
-    }
-}
-
-
 // MARK: - Row
 fileprivate struct ChapterRow: View {
     @Bindable var chapter: SwiftDataChapter
---
+++ DBMultiverse/Sources/MainFeatures/Shared/DynamicSection.swift
@@ -0,0 +1,27 @@
+//
+//  DynamicSection.swift
+//  DBMultiverse
+//
+//  Created by Nikolai Nobadi on 1/4/25.
+//
+
+import SwiftUI
+
+struct DynamicSection<Content: View>: View {
+    let title: String
+    let content: () -> Content
+    
+    init(_ title: String, @ViewBuilder content: @escaping () -> Content) {
+        self.title = title
+        self.content = content
+    }
+    
+    var body: some View {
+        Section {
+            content()
+        } header: {
+            Text(title)
+                .withFont()
+        }
+    }
+}

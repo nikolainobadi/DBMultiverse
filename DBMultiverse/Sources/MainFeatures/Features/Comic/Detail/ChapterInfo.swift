74a44ff634d5ee7f7ed7b3b7dc2394991c795891
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-01-04 20:57:15 -0800

    added unit tests for ChapterComicViewModel

---
+++ DBMultiverse/Sources/MainFeatures/Features/Comic/Detail/ChapterInfo.swift
@@ -0,0 +1,13 @@
+//
+//  ChapterInfo.swift
+//  DBMultiverse
+//
+//  Created by Nikolai Nobadi on 1/4/25.
+//
+
+struct ChapterInfo {
+    let number: Int
+    let startPage: Int
+    let endPage: Int
+    let lastReadPage: Int?
+}
